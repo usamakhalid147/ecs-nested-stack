04990ba26788e2e830d03652aa1ca1f287f37d8a
Fgufran <faisalazuredev@gmail.com>
2023-07-15 02:47:02 +0500

    added code

--- gatewayserver/src/main/java/com/acima/exceptions/exception/GenericException.java
+++
@@ -1,8 +0,0 @@
-package com.acima.exceptions.exception;
-
-public class GenericException extends RuntimeException {
-
-    public GenericException(int statusCode, String message) {
-        super(String.format("Failed for [%s]: %s", statusCode+"", message));
-    }
-}
